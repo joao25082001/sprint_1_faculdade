007ca4a75787c2900a0e185651af454ff4a607dc
dudaettinger <maria.ettinger@sptech.school>
2022-08-24 14:57:45 -0300

    SUBINDO INSERT FUNCIONARIOS

--- BANCO/pronto1.sql
+++ BANCO/pronto1.sql
@@ -38,15 +38,15 @@ alter table empresa modify column cnpj varchar(30);
  ('tintos','55.994.022/0001-08','2000-05-19');
  select * from empresa;
 
- insert into empresa(nome_empresa,cnpj,data_cadastro) values
- ('chadon brasil','77.637.658/0001-55','2022-08-12'),
- ('wine','47.877.611/0001-05','2022-08-12'),
- ('adegavinhos','58.997.155/0001-99','2005-10-10'),
- ('vinhedo','03.987.156/0003-10','2012-08-07'),
- ('anjos_roxo','88.017.100/0001-01','2015-11-03'),
- ('vinhado','09.088.900/0006-08','2011-08-09'),
- ('vinicolas enzo','01.002.100/0031-01','2015-06-03'),
- ('central vinho','69.008.980/0011-01','2015-12-01'),
- ('central vinho','69.008.999/0001-77','2106-02-03'),
- ('tintos','55.994.022/0001-08','2000-05-19');
- select * from empresa;
+ insert into funcionario(nome_func,setor_func,login_func,senha_func) values
+ ('Maria Eduarda Bernardino Ettinger','Financeiro','maria.ettinger@godwine.com','1222000'),
+ ('Vinicios Garcia Fagundes','Administrativo','vinicios.garcia@godwine.com','1222001'),
+ ('Matheus Martins','Marketing','matheus.martins@godwine.com','1222002'),
+ ('Pedro Afonso','Contavel','pedro.afonso@godwine.com','1222003'),
+ ('Gabriel Vilas Boas','Gerente','gabriel.vilas@godwine.com','1222004'),
+ ('Alexandra Zangrando','Recursos Humanos','alexandra.bernardino@godwine.com','1222005'),
+ ('Wagner Oliveira','Suporte Tecnico','wagner.oliveira@godwine.com','1222006'),
+ ('Elisa Ribas','Agricultura','elisa.ribas@godwine.com','1222007'),
+ ('Felipe de Sousa','Abastecimento','felipe.sousa@godwine.com','1222008'),
+ ('Eduardo Damacedo','Pecuária','eduardo.damacedo2godwine.com','1222009');
+ select * from funcionario;
